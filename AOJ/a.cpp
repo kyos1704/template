d7dcbf0814c581a32dff49e740b3cc9c8e868788
kyos <kyos.kyos1704@gmail.com>
2013-06-09 17:48:51 +0900

    add AOJ template

---
+++ AOJ/a.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<climits>
+using namespace std;
+
+
+void init(){
+}
+
+bool input(){
+  return false;
+}
+
+
+
+string solve(){
+  return "No";
+}
+
+
+int main(){
+  while(init(),input()){
+    cout<<solve()<<endl;
+  }
+}
