d044c21afbe54b72e3ff3d4baa8508139ad98910
kyos <kyos.kyos1704@gmail.com>
2013-06-24 21:22:11 +0900

    modify indent

--- Unionfind/a.cpp
+++ Unionfind/a.cpp
@@ -8,38 +8,38 @@ using namespace std;
 
 class union_find{
 private:
-	vector<int> parents;
-	vector<int> rank;
+  vector<int> parents;
+  vector<int> rank;
 public:
-	union_find(int n){
-		parents.resize(n);
-		rank.resize(n);
-		for(int i=0;i<n;i++){
-			parents[i]=i;
-			rank[i]=0;
-		}
-	}
-	int find(int x){
-		if(parents[x]==x){
-			return x;
-		}else{
-			return parents[x]=find(parents[x]);
-		}
-	}
-	void unite(int x,int y){
-		x=find(x);
-		y=find(y);
-		if(x==y)return;
-		if(rank[x]<rank[y]){
-			parents[x]=y;
-		}else{
-			parents[y]=x;
-			if(rank[x]==rank[y])rank[x]++;
-		}
-	}
-	bool same(int x,int y){
-		return (find(x)==find(y));
-	}
+  union_find(int n){
+    parents.resize(n);
+    rank.resize(n);
+    for(int i=0;i<n;i++){
+      parents[i]=i;
+      rank[i]=0;
+    }
+  }
+  int find(int x){
+    if(parents[x]==x){
+      return x;
+    }else{
+      return parents[x]=find(parents[x]);
+    }
+  }
+  void unite(int x,int y){
+    x=find(x);
+    y=find(y);
+    if(x==y)return;
+    if(rank[x]<rank[y]){
+      parents[x]=y;
+    }else{
+      parents[y]=x;
+      if(rank[x]==rank[y])rank[x]++;
+    }
+  }
+  bool same(int x,int y){
+    return (find(x)==find(y));
+  }
 };
 int main(){
 }
